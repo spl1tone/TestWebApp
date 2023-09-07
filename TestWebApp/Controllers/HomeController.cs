d467fb285e605e1f03b719e6fc0f2f8911c060a9
spl1tOne <levonvlad144@gmail.com>
2023-09-07 12:47:47 +0300

    1

--- TestWebApp/Controllers/HomeController.cs
+++ TestWebApp/Controllers/HomeController.cs
@@ -4,6 +4,60 @@ using TestWebApp.Models;
 
 namespace TestWebApp.Controllers
 {
+
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product1", Price = 11f, Count = 5 },
+            new Product { Id = 2, Name = "Product2", Price = 15.99f, Count = 3 },
+            new Product { Id = 3, Name = "Product3", Price = 2.33f, Count = 4 },
+        };
+
+        [HttpGet]
+        public IActionResult GetAllProducts ()
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct (int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null) {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct ([FromBody] Product product)
+        {
+            if (product == null) {
+                return BadRequest("Invalid product data");
+            }
+            product.Id = products.Count + 1;
+            products.Add(product);
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct (int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null) {
+                return NotFound("Product not found");
+            }
+
+            products.Remove(product);
+
+            return NoContent();
+        }
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -24,30 +78,16 @@ namespace TestWebApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private static List<Product> products = new List<Product>();
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product1", Price = 11f, Count = 5 },
+            new Product { Id = 2, Name = "Product2", Price = 15.99f, Count = 3 },
+            new Product { Id = 3, Name = "Product3", Price = 2.33f, Count = 4 },
+        };
 
         public IActionResult Index ()
         {
             return View(products);
-        }
-
-        public IActionResult Create ()
-        {
-            Product newProduct = new Product();
-            return View(newProduct);
-        }
-
-        [HttpPost]
-        public IActionResult Create (Product product)
-        {
-            product.Id = products.Count + 1;
-            products.Add(product);
-            return RedirectToAction("Index");
-        }
-        [HttpPost]
-        public IActionResult AddProduct ()
-        {
-            return RedirectToAction("Create");
         }
 
     }
